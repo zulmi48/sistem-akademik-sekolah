0177e6f9c39f0cd03dacad3c0858750901459a00
Zulmi Fuada <zulmi.f48@gmail.com>
2022-11-11 09:07:00 +0700

    add mapel model,migration, controller, route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MapelController;
 use App\Http\Controllers\SiswaController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::resource('/siswa', SiswaController::class);
+
+Route::resource('/mapel', MapelController::class);
