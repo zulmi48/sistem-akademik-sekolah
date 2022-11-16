a8506c3e0ddac8c771e9af22dcbd0d2942f192ff
Zulmi Fuada <zulmi.f48@gmail.com>
2022-11-16 08:56:30 +0700

    add guru model,migration,controller,route

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
+use App\Http\Controllers\GuruController;
 use App\Http\Controllers\MapelController;
 use App\Http\Controllers\SiswaController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,3 +13,5 @@ Route::get('/', function () {
 Route::resource('/siswa', SiswaController::class);
 
 Route::resource('/mapel', MapelController::class);
+
+Route::resource('/guru', GuruController::class);
